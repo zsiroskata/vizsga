da044e46e4fee8f24f666683dc87021fc169c56f
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2025-01-20 10:26:41 +0100

    Add project files.

---
+++ vizsga/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace vizsga
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Vizsgazo> vizsgazok = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using StreamReader sr = new StreamReader(
+                path: @"..\..\..\src\adatok.txt",
+                encoding: Encoding.UTF8
+                );
+            while (!sr.EndOfStream) 
+            {
+                vizsgazok.Add(new Vizsgazo(sr.ReadLine()));
+            }
+
+            sr.Close();
+        }
+    }
+}
---
+++ vizsga/Vizsgazo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vizsga
+{
+    internal class Vizsgazo
+    {
+        public string Nev { get; set; }
+        public double IT { get; set; }
+        public double Programozas { get; set; }
+        public double HalozatA { get; set; }
+        public double HalozatB { get; set; }
+        public double HalozatC { get; set; }
+        public double HalozatD { get; set; }
+        public double SzobeliAngol { get; set; }
+        public double SzobeliIT { get; set; }
+
+        public Vizsgazo(string sor)
+        {
+            var s = sor.Split(";");
+            Nev = s[0];
+            IT = double.Parse(s[1]);
+            Programozas = double.Parse(s[2]);
+            HalozatA = double.Parse(s[3]);
+            HalozatB = double.Parse(s[4]);
+            HalozatC = double.Parse(s[5]);
+            HalozatD = double.Parse(s[6]);
+            SzobeliAngol = double.Parse(s[7]);
+            SzobeliIT = double.Parse(s[8]);
+        }
+
+        public double vegeredmeny(double szam)
+        {
+            return szam * 100;
+        }
+
+        public string erdemjegy(double jegy)
+        {
+            jegy = jegy * 100;
+            string szoveg= string.Empty;
+            if (jegy >= 81 && jegy <= 100)
+            {
+                szoveg = "jeles";
+            }
+            else if (jegy >= 71 && jegy <= 80)
+            {
+                szoveg = "jó";
+            }
+            else if (jegy >= 61 && jegy <= 70)
+            {
+                szoveg = "közepes";
+            }
+            else if (jegy >= 51 && jegy <= 60)
+            {
+                szoveg = "elégséges";
+            }
+            else 
+            {
+                szoveg = "elégedetlen";
+            }
+
+
+            return szoveg;
+        }
+    }
+}
